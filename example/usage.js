8d773be6d813cce5db874310ac95e410125f1866
Jason Pincin <jason@pincin.com>
2014-05-24 22:33:51 -0400

    Corrected conventions in usage example

--- example/usage.js
+++ example/usage.js
@@ -1,25 +1,25 @@
-var createTree = require('..')
-,   assert = require('assert')
-;
+var createTree = require('..'),
+    assert = require('assert')
 
-var tree = createTree();
 
-tree.createStream().pipe(process.stdout); // {op: 'create', pnode: {...} }
+var tree = createTree()
 
-var task1 = tree('/tasks/1');
+tree.createStream().pipe(process.stdout) // {op: 'create', pnode: {...} }
 
-task1.set('crawl');
+var task1 = tree('/tasks/1')
 
-tree('/tasks/2').set('walk');
-tree('/tasks/3').set('run');
+task1.set('crawl')
 
-task1.child('status').set('completed');
+tree('/tasks/2').set('walk')
+tree('/tasks/3').set('run')
 
-var tasks = tree('/tasks').children;
-assert(tasks.length === 3);
-assert(tree.pnode.isPrototypeOf(tasks[0]));
-assert(tasks[0].data === 'crawl');
-assert(tasks[0].children[0].name === 'status');
+task1.child('status').set('completed')
 
-task1.child('status').remove();
-assert(task1.children.length === 0);
+var tasks = tree('/tasks').children
+assert(tasks.length === 3)
+assert(tree.pnode.isPrototypeOf(tasks[0]))
+assert(tasks[0].data === 'crawl')
+assert(tasks[0].children[0].name === 'status')
+
+task1.child('status').remove()
+assert(task1.children.length === 0)
